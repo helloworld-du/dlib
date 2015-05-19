e606c10670577ad0d333ab6fcdd45502051d89f8
dushengchen <dushengch100@126.com>
2015-05-19 19:23:48 +0800

    auto loader

---
+++ test/script/autoLoader.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dushengchen
+ * Date: 15/5/18
+ * Time: 下午6:01
+ */
+require __DIR__.'/../../Commen.inc.php';
+var_dump($res = require LIB_ROOT.'vendor/composer/autoload_psr4.php');
