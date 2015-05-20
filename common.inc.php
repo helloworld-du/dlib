6e8216c0f536cddf5298be6e7d64fe4fdc63ab82
dushengchen <dushengch100@126.com>
2015-05-20 10:43:46 +0800

    add spl_autoload_register

--- common.inc.php
+++ common.inc.php
@@ -20,15 +20,14 @@ require LIB_ROOT.'vendor/autoload.php';
 
 
 
-//spl_autoload_register(function ($class) {
-//
-//	if (strpos($class, 'dlib\\') === 0) {
-//		$class = str_replace('dlib\\', '',$class);
-//	}
-//	$class = str_replace('\\', '/',$class);
-//
-//	$sRealPath = ROOT.str_replace('_', '/',$class).'.php';
-//	if (file_exists($sRealPath)) {
-//		require $sRealPath;
-//	}
-//}, true, true);
+spl_autoload_register(function ($class) {
+
+	if (strpos($class, 'dlib\\') === 0) {
+		$class = str_replace('dlib\\', '',$class);
+		$class = str_replace('\\', '/',$class);
+		$sRealPath = LIB_ROOT.str_replace('_', '/',$class).'.php';
+		if (file_exists($sRealPath)) {
+			require $sRealPath;
+		}
+	}
+}, true, true);
